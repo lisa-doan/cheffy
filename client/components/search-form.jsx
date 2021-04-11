4366d5de2e19d8011f9765594cc340c63037033c
lisadoann <ldoan24@gmail.com>
2021-04-11 03:51:05 +0000

    Removed onClick method, add if statement to onSubmit, add results container

--- client/components/search-form.jsx
+++ client/components/search-form.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import RecipeCard from './recipe-card';
 
 const apiId = process.env.REACT_APP_API_ID;
 const apiKey = process.env.REACT_APP_API_KEY;
@@ -8,7 +9,7 @@ export default class SearchForm extends React.Component {
     super(props);
     this.state = {
       value: '',
-      data: []
+      results: []
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -20,40 +21,48 @@ export default class SearchForm extends React.Component {
 
   handleSubmit(event) {
     event.preventDefault();
-    const input = this.state.value;
-    fetch(`https://api.edamam.com/search?app_id=${apiId}&app_key=${apiKey}&q=${input}`)
+    let input = '';
+    if (event.target.value) {
+      input = event.target.value;
+    } else {
+      input = this.state.value;
+    }
+    fetch(`https://api.edamam.com/search?app_id=${apiId}&app_key=${apiKey}&from=0&to=9&q=${input}`)
       .then(response => response.json())
-      .then(result => {
-        console.log(result);
-      })
-      .catch(error => console.error(error));
-  }
-
-  handleClick(event) {
-    const input = event.target.value;
-    fetch(`https://api.edamam.com/search?app_id=${apiId}&app_key=${apiKey}&q=${input}`)
-      .then(response => response.json())
-      .then(result => {
-        console.log(result);
+      .then(data => {
+        this.setState({ results: data.hits });
       })
       .catch(error => console.error(error));
   }
 
   render() {
+    console.log('state.results: ', this.state.results);
+
     return (
       <>
-      <div className="search-bar-container">
-        <i className="fa fa-search search-icon"></i>
-        <form onSubmit={this.handleSubmit}>
+      <div className="search-content">
+        <form className="search-bar-container" onSubmit={this.handleSubmit}>
+          <i className="fa fa-search search-icon"></i>
           <input className="search-box" type="search" placeholder="Search" value={this.state.value} onChange={this.handleChange} ></input>
         </form>
-      </div>
       <div className="button-container">
-        <button className="blue-button" value="breakfast" onClick={this.handleClick}>breakfast</button>
-        <button className="blue-button" value="lunch" onClick={this.handleClick}>lunch</button>
-        <button className="blue-button" value="dinner" onClick={this.handleClick}>dinner</button>
-        <button className="blue-button" value="dessert" onClick={this.handleClick}>dessert</button>
+        <button className="blue-button" value="breakfast" onClick={this.handleSubmit}>breakfast</button>
+        <button className="blue-button" value="lunch" onClick={this.handleSubmit}>lunch</button>
+        <button className="blue-button" value="dinner" onClick={this.handleSubmit}>dinner</button>
+        <button className="blue-button" value="dessert" onClick={this.handleSubmit}>dessert</button>
       </div>
+      </div>
+      <div className="results-container">
+        {this.state.results.map(recipe => {
+          return (
+          <RecipeCard
+            key={recipe.recipe.label}
+            title={recipe.recipe.label}
+            img={recipe.recipe.image}
+          />
+          );
+        })}
+        </div>
       </>
     );
   }
