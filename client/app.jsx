865ed0d5e2d7862a18952ea8b2b71129f4902507
lisadoann <ldoan24@gmail.com>
2021-04-14 03:11:34 +0000

    Added hash routing

--- client/app.jsx
+++ client/app.jsx
@@ -1,16 +1,39 @@
 import React from 'react';
-import Home from './pages/home';
 import AppDrawer from './components/app-drawer';
 import Search from './pages/search';
+import RecipeDetails from './pages/recipe-details';
+import parseRoute from './lib/parse-route';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      route: parseRoute(window.location.hash)
+    };
+  }
+
+  componentDidMount() {
+    window.addEventListener('hashchange', () => {
+      this.setState({ route: parseRoute(location.hash) });
+    });
+  }
+
+  renderPage() {
+    const { route } = this.state;
+    if (route.path === '') {
+      return <Search />;
+    }
+    if (route.path === 'recipes') {
+      const recipeId = route.params.get('recipeId');
+      return <RecipeDetails recipeId={recipeId} />;
+    }
+  }
 
   render() {
     return (
       <div className="container">
     <AppDrawer />
-    <Search />
-
+    {this.renderPage()}
     </div>
     );
   }
