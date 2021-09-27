d4ffb70597d1360b644ff732ea3167ed673d06aa
Lisa Doan <ldoan24@gmail.com>
2021-09-27 15:47:37 -0700

    Revert "Issue fixes 2"

--- client/app.jsx
+++ client/app.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Search from './pages/search';
 import Recipe from './pages/recipe';
 import Cookbook from './pages/cookbook';
+import Timer from './pages/timer';
 import parseRoute from './lib/parse-route';
 
 export default class App extends React.Component {
@@ -25,6 +26,9 @@ export default class App extends React.Component {
     }
     if (route.path === 'cookbook') {
       return <Cookbook />;
+    }
+    if (route.path === 'timer') {
+      return <Timer />;
     }
     if (route.path === 'recipes') {
       const recipeId = route.params.get('recipeId');
